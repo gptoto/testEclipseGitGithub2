834a56f6092bc113022147433346ccd724aa7286
gptoto <hamonaxe@live.fr>
2021-01-29 15:56:36 +0100

    Modification de App

--- src/main/java/EclipseGitGithub2/EclipseGitGithub2/App.java
+++ src/main/java/EclipseGitGithub2/EclipseGitGithub2/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Bonjour EPSI 4....!" );
+        System.out.println( "Bonjour EPSI 5....!" );
     }
 }
